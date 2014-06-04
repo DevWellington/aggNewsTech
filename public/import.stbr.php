2b3b44baaabbb689be2ee9ba5064938cd54316ee
Wellington Ribeiro <devwellington@gmail.com>
2014-06-04 01:19:32 -0300

    update functions to import db

--- public/import.stbr.php
+++ public/import.stbr.php
@@ -8,40 +8,9 @@ require_once "../lib/external/phpQuery.php";
 require_once "../lib/conexao.pdo.php";
 require_once "../lib/class/cURL.php";
 require_once "../lib/class/import_STBR.php";
+require_once "../lib/class/import_STBR_DB.php";
 
 $html = cURL::exec_cURL("http://www.startupbrasil.org.br/agenda/");
-$arAgenda = Import_STBR::getArrayData($html);
 
-echo "<pre>";
-
-$cxPDO = connPDO::getInstance();
-
-foreach ($arAgenda as $field => $item) {
-
-    $obj = (object) $item;
-
-    $stmt = $cxPDO->prepare("INSERT INTO tbl_newsStartUP (title, linkImg, location, linkReadMore, dtEvent, dt_Insert)
-                             VALUES (:title, :linkImg, :location, :linkReadMore, :dtEvent, now())");
-
-    $stmt->bindParam(':title', $obj->title);
-    $stmt->bindParam(':linkImg', $obj->linkImg);
-    $stmt->bindParam(':location', $obj->location);
-    $stmt->bindParam(':linkReadMore', $obj->linkReadMore);
-    $stmt->bindParam(':dtEvent', $obj->dtEvent);
-
-    $return = $stmt->execute();
-
-    if ($return){
-        echo $obj->title.PHP_EOL;
-    } else {
-        $arError = $stmt->errorInfo();
-        echo "ERROR - ".$arError[2].PHP_EOL;
-    }
-
-    flush();
-    ob_flush();
-
-}
-echo "</pre>";
-// fecho o banco
-$cxPDO = null;
+import_STBR::getArrayData($html);
+import_STBR_DB::insertToDB();
